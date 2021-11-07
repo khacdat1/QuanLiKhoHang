852ec638ff5076e9794e75ad56056ecebdcd5804
khacdat1 <88232628+khacdat1@users.noreply.github.com>
2021-11-07 20:23:44 +0700

    Update HangHoa.h

--- HangHoa.h
+++ HangHoa.h
@@ -1,28 +1,28 @@
-// #pragma once      
-// #include <iostream>
-// #include <string>
-// #include <fstream>
-// #include "NhaCungCap.h"
-// #include "LoaiHangHoa.h"
-// using namespace std;
-// class HangHoa
-// {   
-//     string _IDHangHoa;
-//     string _IDNhaCungCap;
-//     string _TenHangHoa;
-//     string _IDLoaiHangHoa;
-//     double _SoLuong;
-//     double _DonGia;
-//     string _NgayNhapXuat;
-//     string HanSuDung;
-//     public :
-//         static FileIn;
-//         static FileOut;
-//     public:
-//         HangHoa(string="",NhaCungCap::GetIDNCC(),string= " ",LoaiHangHoa::GetIDLHH(),double= 0.0,double= 0.0,string= "",string="");
-//         ~HangHoa();
-//         void Show();
-//         void SetNgayNhapXuat(const string&);
-//         void SetSoLuong(const string&);
-//         bool ShowHanSuDung();
-// };
+#pragma once      
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "NhaCungCap.h"
+#include "LoaiHangHoa.h"
+using namespace std;
+class HangHoa
+{   
+    string _IDHangHoa;
+    string _IDNhaCungCap;
+    string _TenHangHoa;
+    string _IDLoaiHangHoa;
+    double _SoLuong;
+    double _DonGia;
+    string _NgayNhapXuat;
+    string HanSuDung;
+    public :
+        static FileIn;
+        static FileOut;
+    public:
+        HangHoa(string="",NhaCungCap::GetIDNCC(),string= " ",LoaiHangHoa::GetIDLHH(),double= 0.0,double= 0.0,string= "",string="");
+        ~HangHoa();
+        void Show();
+        void SetNgayNhapXuat(const string&);
+        void SetSoLuong(const string&);
+        bool ShowHanSuDung();
+};
