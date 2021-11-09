70ea4da4e034e87ee808f3a9c48a7ebc433c8c49
khacdat1 <88232628+khacdat1@users.noreply.github.com>
2021-11-09 07:46:37 +0700

    Update HoaDon.h

--- HoaDon.h
+++ HoaDon.h
@@ -1,1 +1,33 @@
+#include<string>
+#include"HangHoa.h"
+#include<fstream>
+using namespace std;
+class HoaDon /*:public HangHoa*/
+{
+    private:
+        string IdHoadon;
+        string IdNhanvien;
+        string IdNhacungcap;
+        string IdHanghoa;
+        int Soluong;
+        double Thanhtien;
+        double DonGia;
+        string Trangthai;
+        string Ngaynhap;
+        string Ngayxuat;
+    public:
+        HoaDon();
+        ~HoaDon();
+        void TinhTien();
+        int getSoluong();
+        void setSoluong();
+        int getDonGia();
+        void setDonGia();
+        double getThanhtien();
+        void TaoHoaDon();
+        // void seachIdHanghoa();
 
+        
+
+
+};
