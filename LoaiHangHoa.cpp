407f3de73f51b9a9abc8a8aca4de0f6447e96240
khacdat1 <88232628+khacdat1@users.noreply.github.com>
2021-11-12 20:32:52 +0700

    Update LoaiHangHoa.cpp

--- LoaiHangHoa.cpp
+++ LoaiHangHoa.cpp
@@ -1,8 +1,7 @@
-#pragma 
 #include <iostream>
 using namespace std;
 #include "LoaiHangHoa.h"
-LoaiHangHoa::LoaiHangHoa(string ID,string Ten) :_IDLoaiHangHoa(ID),_TenLoaiHangHoa(Ten)
+LoaiHangHoa::LoaiHangHoa(string ID, string Ten) :_IDLoaiHangHoa(ID), _TenLoaiHangHoa(Ten)
 {
 
 }
@@ -12,18 +11,22 @@ LoaiHangHoa::~LoaiHangHoa()
 }
 void LoaiHangHoa::Nhap()
 {
-    cout<<"Nhap ID : ";
-    getline(cin,_IDLoaiHangHoa);
-    cout<<"Nhap ten loai : ";
-    getline(cin,_TenLoaiHangHoa);
+   cout << "Nhap ID : ";
+   getline(cin, _IDLoaiHangHoa);
+   cout << "Nhap ten loai : ";
+   getline(cin, _TenLoaiHangHoa);
 }
 void LoaiHangHoa::Xuat()
 {
-    cout<<"ID Loai hang hoa = "<<_IDLoaiHangHoa<<endl;
-    cout<<"Ten loai hang hoa = "<<_TenLoaiHangHoa<<endl;
+   cout << "ID Loai hang hoa = " << _IDLoaiHangHoa << endl;
+   cout << "Ten loai hang hoa = " << _TenLoaiHangHoa << endl;
 }
 string LoaiHangHoa::GetIDLHH()
 {
-    return this->_IDLoaiHangHoa;
+   return this->_IDLoaiHangHoa;
+}
+string LoaiHangHoa::GetTen()
+{
+   return this->_TenLoaiHangHoa;
 }
 
