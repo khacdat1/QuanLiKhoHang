45fac8b3855066da2a15662e7eb0fd28a77d9859
khacdat1 <88232628+khacdat1@users.noreply.github.com>
2021-11-12 20:34:46 +0700

    Update NhanVien.h

--- NhanVien.h
+++ NhanVien.h
@@ -1,17 +1,20 @@
-#pragma
+#pragma once
 #include<string>
 #include"NCC_NV.h"
 using namespace std;
 class NhanVien : public NCC_NV
 {
-    private:
-        string _Ca;
-        string _Chucvu;
-    public:
-        NhanVien(string ="",string = "",string = "",string = "");
-        ~NhanVien();
-        void Nhap();
-        void Xuat();
-        string GetCa();
-        string GetChucVu();
+private:
+    string _CaLam;
+    string _ChucVu;
+public:
+    NhanVien(string = "", string = "", string = "", string = "");
+    ~NhanVien();
+    void Nhap(fstream&);
+    void Xuat(fstream&);
+    string GetCaLam();
+    string GetChucVu();
+    void SetCaLam(string);
+    void SetChucVu(string);
 };
+
