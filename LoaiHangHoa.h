31fe52fd3396e5b5efab7ca25836c5b1edb63acc
khacdat1 <88232628+khacdat1@users.noreply.github.com>
2021-11-12 20:33:11 +0700

    Update LoaiHangHoa.h

--- LoaiHangHoa.h
+++ LoaiHangHoa.h
@@ -1,17 +1,18 @@
-#pragma
+#pragma once
 #include <string>
 #include "NCC_NV.h"
 using namespace std;
-class LoaiHangHoa 
+class LoaiHangHoa
 {
-    private : 
-        string _IDLoaiHangHoa;
-        string _TenLoaiHangHoa;
-    public:
-        LoaiHangHoa(string = " ", string = " ");
-        ~LoaiHangHoa();
-        void Nhap();
-        void Xuat();
-        string GetIDLHH();
+private:
+   string _IDLoaiHangHoa;
+   string _TenLoaiHangHoa;
+public:
+   LoaiHangHoa(string = " ", string = " ");
+   ~LoaiHangHoa();
+   void Nhap();
+   void Xuat();
+   string GetIDLHH();
+   string GetTen();
 };
 
