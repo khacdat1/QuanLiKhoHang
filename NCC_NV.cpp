0a2853ac92004fcba681adbd7dbe11a832ba275e
khacdat1 <88232628+khacdat1@users.noreply.github.com>
2021-11-12 20:33:51 +0700

    Update NCC_NV.cpp

--- NCC_NV.cpp
+++ NCC_NV.cpp
@@ -1,43 +1,39 @@
-#pragma
 #include <iostream>
 #include "NCC_NV.h"
-ifstream NCC_NV::FileIn;
-ofstream NCC_NV::FileOut;
-NCC_NV::NCC_NV(string ten,string id) : _Ten(ten),_ID(id)
-{
 
-}
-NCC_NV::~NCC_NV()
+//NCC_NV::NCC_NV()
+//{
+//
+//}
+//NCC_NV::~NCC_NV()
+//{
+//
+//}
+void NCC_NV::Nhap(fstream& File)
 {
-
+    /*FileIn.open("input.txt");*/
+    getline(File, this->_Ten, ',');
+    File.seekg(1, SEEK_CUR);
+    getline(File, this->_ID, ',');
+    File.seekg(1, SEEK_CUR);
+    /*FileIn.close();*/
 }
-void NCC_NV::Nhap()
+void NCC_NV::Xuat(fstream& File)
 {
-    cout<<"Nhap ID = ";
-    getline(cin,_ID);
-    cout<<"Nhap ten = ";
-    getline(cin,_Ten);
+    /*FileOut.open("output.txt", ios::out);*/
+    File << "ID  = " << _ID << endl;
+    File << "Ten = " << _Ten << endl;
+   /* FileOut.close();*/
 }
-void NCC_NV::Xuat()
-{
-    cout<<"ID  = "<<_ID<<endl;
-    cout<<"Ten = "<<_Ten<<endl;
-}
-// void NCC_NV::Nhap()
-// {
-//     FileIn.open("input.txt");
-//     getline(FileIn,_Ten,',');
-//     FileIn.seekg(0,ios_base::cur);
-//     getline(FileIn,_ID,',');
-//     FileIn.seekg(1,ios_base::cur);
-//     FileIn.close();
-// }
-// void NCC_NV::Xuat()
-// {
-//     FileOut.open("output.txt",ios::out);
-//     FileOut<<"ID  = "<<_ID;
-//     FileOut<<"Ten = "<<_Ten<<endl;
-//     cout<<"ID  = "<<_ID;
-//     cout<<"Ten = "<<_Ten<<endl;
-//     FileOut.close();
-// }
+ //void NCC_NV::Nhap()
+ //{
+ //    cout<<"Nhap ID = ";
+ //    getline(cin,_ID);
+ //    cout<<"Nhap ten = ";
+ //    getline(cin,_Ten);
+ //}
+ //void NCC_NV::Xuat()
+ //{
+ //    cout<<"ID  = "<<_ID<<endl;
+ //    cout<<"Ten = "<<_Ten<<endl;
+ //}
