4a87cf0741ba40f95e5e923e59b66396d2cf5240
khacdat1 <88232628+khacdat1@users.noreply.github.com>
2021-11-12 20:31:44 +0700

    Update HangHoa.h

--- HangHoa.h
+++ HangHoa.h
@@ -2,27 +2,41 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include "NhaCungCap.h"
-#include "LoaiHangHoa.h"
+#include "QuanLy.h"
 using namespace std;
-class HangHoa
-{   
+class HangHoa 
+{
+protected:
     string _IDHangHoa;
     string _IDNhaCungCap;
     string _TenHangHoa;
     string _IDLoaiHangHoa;
     double _SoLuong;
     double _DonGia;
-    string _NgayNhapXuat;
-    string HanSuDung;
-    public :
-        static FileIn;
-        static FileOut;
-    public:
-        HangHoa(string="",NhaCungCap::GetIDNCC(),string= " ",LoaiHangHoa::GetIDLHH(),double= 0.0,double= 0.0,string= "",string="");
-        ~HangHoa();
-        void Show();
-        void SetNgayNhapXuat(const string&);
-        void SetSoLuong(const string&);
-        bool ShowHanSuDung();
+    string _NgayNhap;
+    string _HanSuDung;
+public:
+    HangHoa(string = "", string = "", string = "", string = "",double = 0,double=0, string = "", string = "");
+    ~HangHoa();
+    void SetIDHangHoa(const string&);
+    string GetIDHangHoa();
+    void SetTenHangHoa(const string&);
+    string GetTenHangHoa();
+    void SetSoLuong(const double&);
+    double GetSoLuong();
+    void SetDonGia(const double&);
+    double GetDonGia();
+    void SetNgayNhap(const string&);
+    string GetNgayNhap();
+    void  HanSuDung(const string&);
+    string GetHanSuDung();
+    void Xuat();
+    void Nhap();
+   // void ADD();
+   // void Show();
+   /* void Update();
+    void Delete();
+    string Search();*/
+    void NhapFile(fstream&);
+    void XuatFile(fstream&);
 };
