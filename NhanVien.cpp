ae87e39d223aa77b64e4fe9793a851ffd4ca8464
khacdat1 <88232628+khacdat1@users.noreply.github.com>
2021-11-12 20:35:00 +0700

    Update NhanVien.cpp

--- NhanVien.cpp
+++ NhanVien.cpp
@@ -1,52 +1,57 @@
 #include<iostream>
 #include<string>
 #include"NhanVien.h"
-#include "NCC_NV.h"
 using namespace std;
-NhanVien::NhanVien(string ID,string Ten,string Ca,string Chucvu) : NCC_NV()
+NhanVien::NhanVien(string ID, string Ten, string CaLam, string Chucvu) : NCC_NV()
 {
-    this->_Ca=Ca;
-    this->_Chucvu=Chucvu;
+    this->_CaLam = CaLam;
+    this->_ChucVu = Chucvu;
 }
 NhanVien::~NhanVien()
 {
 
 }
-void NhanVien::Nhap()
+void NhanVien::Nhap(fstream& File)
 {
-    cout<<"Nhan vien :"<<endl;
-    NCC_NV::Nhap();
-    cout<<"Nhap ca lam viec (sang/toi) : ";
-    getline(cin,_Ca);
-    cout<<"Nhap chuc vu (nhan vien,quan li) :";
-    getline(cin,_Chucvu);
+    NCC_NV::Nhap(File);
+    getline(File, this->_CaLam, ',');
+    File.seekg(1, SEEK_CUR);
+    getline(File, this->_ChucVu);
 }
-void NhanVien::Xuat()
-{
-    cout<<"ID nhan vien :"<<_ID<<endl;
-    cout<<"Ten nhan vien : "<<_Ten<<endl;
-    cout<<"Ca lam viec : "<<_Ca<<endl;
-    cout<<"Chuc vu :"<<_Chucvu<<endl;
-}
+// void NhanVien::Xuat()
+// {
+//     cout<<"ID nhan vien :"<<_ID<<endl;
+//     cout<<"Ten nhan vien : "<<_Ten<<endl;
+//     cout<<"Ca lam viec : "<<_Ca<<endl;
+//     cout<<"Chuc vu :"<<_Chucvu<<endl;
+// }
 // void NhanVien::Nhap()
 // {
 //     NCC_NV::Nhap();
 //     getline(FileIn,_Ca,) //xem lại chỗ này 
 //     getline(FileIn,_Chucvu,)
 // }
-// void NhanVien::Xuat()
-// {
-//     NCC_NV::Xuat();
-//     NCC_NV::FileOut<<"Ca = "<<_Ca<<endl;
-//     cout<<"Ca = "<<_Ca<<endl;
-//     NCC_NV::FileOut<<"Chuc vu = "<<_Chucvu<<endl;
-//     cout<<"Chuc vu = "<<_Chucvu<<endl;
-// }
-string NhanVien::GetCa()
+void NhanVien::Xuat(fstream& File)
 {
-    return this->_Ca;
+    File << "Nhan vien : " << endl;
+    NCC_NV::Xuat(File);
+    File << "Ca lam = " << _CaLam << endl;
+    File << "Chuc vu = " << _ChucVu << endl;
+    File << "\n";
+}
+string NhanVien::GetCaLam()
+{
+    return this->_CaLam;
 }
 string NhanVien::GetChucVu()
 {
-    return this->_Chucvu;
+    return this->_ChucVu;
+}
+void NhanVien::SetCaLam(string CaLam)
+{
+    this->_CaLam = CaLam;
+}
+void NhanVien::SetChucVu(string ChucVu)
+{
+    this->_ChucVu = ChucVu;
 }
